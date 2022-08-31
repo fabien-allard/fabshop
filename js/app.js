45c1c40aeb166282da2f37758e4730ce30f4cbd6
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-08-31 17:09:36 +0200

    Add files via upload
    ajout du fichier app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+(function($){
 
+    $('.addPanier').click(function(event){
+            event.preventDefault();
+            $.get($(this).attr('href'),{},function(data){
+                if(data.error){
+                    alert(`${data.messsage}. Le Produit à bien était ajouté a votre panier`);
+                }else{
+                    if(confirm(data.messsage + '. Voulez vous consulter votre panier ?')){
+                        location.href = 'panier.php';
+                    }else{
+                        $('#total').empty().append(data.total);
+                        $('#count').empty().append(data.count);
+                    }
+                }
+            },'json');
+            return false;
+    });
+})(jQuery);
